db25f76495e7bd7fce1d7da1a032555bf6ac863c
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2024-10-22 16:06:50 -0400

    new url setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';  // Import the HomeComponent
 
-
 export const routes: Routes = [
     { path: '', component: HomepageComponent },  // Default route (Home)
     { path: '**', redirectTo: '', pathMatch: 'full' }  // Fallback to Home if no route is found
-
 ];
+
