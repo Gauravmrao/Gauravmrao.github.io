526b850cde8f0cde2367f91be299ee11b7e2b713
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2024-11-13 13:25:24 -0500

    made some very clunky changes to root stylesheet, but getting an idea of the overall theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';  // Import the HomeComponent
 import { AboutComponent } from './components/about/about.component';
+import { WorkComponent } from './components/work/work.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },  // Default route (Home)
     { path: 'about', component: AboutComponent }, // Route for the About page
+    { path: 'work', component: WorkComponent}, // Route for the Projects and Work page
     { path: '**', redirectTo: '', pathMatch: 'full' }  // Fallback to Home if no route is found
 ];
 
